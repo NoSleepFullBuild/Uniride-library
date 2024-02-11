2b172d0a527d3ceef69db57933df3a73fc3a490c
MTthoas <matthias.pecquery@gmail.com>
2024-02-11 22:05:13 +0100

    (fix) fix export index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 
 import authModel from './src/models/authModel';
 import userModel from './src/models/userModel';
+import { checkoutUsername, checkoutEmail, checkoutPassword, checkoutPhoneNumber, checkoutRole, checkoutVehicle } from './src/middleware/authentification';
 
 export { authModel, userModel };
 export { BaseEntity } from './src/entity/base.entity';
+export { checkoutUsername, checkoutEmail, checkoutPassword, checkoutPhoneNumber, checkoutRole, checkoutVehicle };
+
 
